8491337732b3f20e73a98fc387760bd2c5cb724c
koshatnitca@pc <koshatnitca_21@mail.ru>
2018-12-16 14:22:02 +0700

    Problems: words with '.' and last word in the dictionary

--- case_study7.py
+++ case_study7.py
@@ -1,3 +1,4 @@
+
 """ Case-study №7
 Developers:
 Zelenskaya A. , Zolotykh M. , Sherubneva A.
@@ -27,6 +28,7 @@ try:
         char = '.!?'
         start_words =[]
         stop_words = []
+
         for i in text:
             if i[-1] in char:
                 i = i.replace(i[-1],'')
@@ -38,12 +40,7 @@ try:
         for q in text:
             if q[-1] in char:
                 stop_words.append(q)
-        number = int(input('Number of sentences generated: '))
-        for i in range(number):
-            first_word = random.choice(start_words)
-            last_word = random.choice(stop_words)
 
-        # часть кода со словарем
         a = text
         b = []
         for i in a:
@@ -52,21 +49,53 @@ try:
                 continue
             else:
                 b.append(i)
-        dict = {}
+        diction = {}
         for e in b:
-            dict[e] = []
-        print(dict)
-        for t in dict:
+            diction[e] = []
+        for t in diction:
             for q in a[0:-1]:
                 if q == t:
                     w = a.index(q)
                     w += 1
                     k = a[w]
-                    dict[t].append(k)
+                    diction[t].append(k)
                     a.remove(q)
-        print(dict)
 
+        number = int(input('Number of sentences generated: '))
+        j = sorted(diction.keys())
+        j_1 = j[-1]
 
+        # d_1 = diction.pop(j_1)
+        print(diction)
+
+        for i in range(number):
+            first_word = random.choice(start_words)
+            f_l = diction[first_word]
+            r = random.choice(f_l)
+            print(first_word, end = ' ')
+            le = 0
+            qw = 1
+            while qw < 19 and le == 0:
+                key_word = r
+                a_w_l = diction[key_word]
+                add_word = random.choice(a_w_l)
+                if key_word in stop_words:
+                    if qw >= 5:
+                        le = 1
+                    if qw < 5:
+                        while key_word in stop_words:
+                            key_word = random.choice(j)
+                            a_w_l = diction[key_word]
+                            add_word = random.choice(a_w_l)
+                if key_word in stop_words:
+                    print(key_word, end=' ')
+                else:
+                    print(key_word, end=' ')
+                qw += 1
+                r = add_word
+            if le == 0:
+                last_word = random.choice(stop_words)
+                print(last_word, end=' ')
 
 except FileNotFoundError:
     print('File {} not found.'.format(file))
